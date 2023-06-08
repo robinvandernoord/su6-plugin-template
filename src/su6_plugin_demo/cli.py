4ff5573b8e609918951fdee10dad61e190d4bc08
Robin van der Noord <robinvandernoord@gmail.com>
2023-06-08 14:16:28 +0200

    refactor: stripped away more demo stuff

--- src/su6_plugin_demo/cli.py
+++ src/su6_plugin_demo/cli.py
@@ -5,19 +5,7 @@ import typing
 
 from su6.plugins import PluginConfig, print, register, run_tool
 
-# or: from su6 import register_plugin, run_tool
 from typer import Typer
-
-# method 1: adding top-level commands
-
-
-@register(with_state=True, config_key="demo.extra")
-class MoreDemoConfig(PluginConfig):
-    """
-    Config that loads 'state' into self.state and loads [tool.su6.demo.extra] from pyproject.toml into self.
-    """
-
-    more: bool
 
 
 @register
@@ -27,13 +15,13 @@ class DemoConfig(PluginConfig):
     """
 
     required_arg: str
-    boolean_arg: bool
-    optional_with_default: typing.Optional[str] = None
-    more: PluginConfig = MoreDemoConfig(more=False)
+    # etc.
 
 
 config = DemoConfig()
 
+
+# method 1: adding top-level commands
 
 @register
 def first() -> int:
@@ -46,46 +34,7 @@ def first() -> int:
     return 0
 
 
-@register()
-def second() -> int:
-    """
-    Register a top-level command.
-
-    @register also works with ()
-    """
-    print("This is another demo command (with exit code)!")
-    run_tool("echo", "args", "go", "here")
-    return 1
-
-
-@register(name="third")
-def yet_another() -> bool:
-    """
-    Register a top-level command.
-
-    @register works with extra Typer arguments.
-    """
-    print("This is another demo command (with bool exit)!")
-    return True
-
-
-@register()
-def with_arguments(required_arg: str, boolean_arg: bool = False) -> None:
-    """
-    Register a top-level command.
-
-    @register works with extra Typer arguments.
-    """
-    config.update(required_arg=required_arg, boolean_arg=boolean_arg)
-    print(config)
-    assert (
-        config.more.state
-        and config.more.state.config
-        and config.more.extras["state"].config
-        and config.more.extras["state"]
-        and config.more.state.config.pyproject == config.more.extras["state"].config.pyproject
-    )
-
+# etc.
 
 # method 2: adding a namespace (based on the plugin package name)
 
@@ -100,3 +49,5 @@ def subcommand() -> None:
     Can be used as `su6 demo subcommand` (in this case, the plugin name is demo)
     """
     print("this lives in a namespace")
+
+# etc.
